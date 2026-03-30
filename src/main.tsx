c4cb6d62b4c9f5e56b6650b1a947568227874e9f
848bruno <110998389+848bruno@users.noreply.github.com>
2026-03-30 15:21:54 +0300

    Wrap App component with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App.tsx';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
